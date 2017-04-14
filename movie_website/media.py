fffa34d3b0d7a4e599ce38fc0c2b2a1e710ca429
OnlyStu <371706801@qq.com>
2017-04-14 11:46:11 -0700

    删除中文字符

--- movie_website/media.py
+++ movie_website/media.py
@@ -1,6 +1,6 @@
 
 class Movie:
-    ''' properties has title、poster_image_url、trailer_youtube_url '''
+    ''' properties has title poster_image_url trailer_youtube_url '''
     def __init__(self,movie_title,poster_image,trailer_yutube):
         self.title = movie_title
         self.poster_image_url = poster_image
